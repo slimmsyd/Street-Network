8244abf975d3cf43db01bce56cd041d7a8d21b15
slimmsyd <ssanderss444@gmail.com>
2025-01-28 04:55:53 -0500

    updatd this jiont

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -27,6 +27,15 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: require.resolve('crypto-browserify'),
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
