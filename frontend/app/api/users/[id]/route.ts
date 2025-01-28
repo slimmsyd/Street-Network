bf1a65a1a928a25454cb06643af79b6f191c3b06
slimmsyd <ssanderss444@gmail.com>
2025-01-28 03:10:26 -0500

    rmoeveed the icion

--- frontend/app/api/users/[id]/route.ts
+++ frontend/app/api/users/[id]/route.ts
@@ -1,44 +1,5 @@
 import { NextResponse } from 'next/server';
-import mongoose from 'mongoose';
-import User from '../../../../models/User';
-import { uploadMilestoneToArDrive } from '../../../../lib/ardriveUtils';
-import path from 'path';
-import fs from 'fs';
-import { execSync } from 'child_process';
-import Arweave from 'arweave';
 import { MongoClient, ObjectId } from "mongodb";
-import { getServerSession } from "next-auth";
-import { authOptions } from '@/lib/auth';
-
-interface UpdateQuery {
-  name?: any;
-  occupation?: any;
-  email?: any;
-  phoneNumber?: any;
-  birthDay?: any;
-  maritalStatus?: any;
-  location?: any;
-  bio?: any;
-  familyRole?: any;
-  interests?: any;
-  contributions?: any;
-  settings?: any;
-  profileImage?: any;
-  milestones?: Array<{
-    _id: string;
-    date: string;
-    title: string;
-    description: string;
-  }>;
-}
-
-interface Milestone {
-  date: string;
-  title: string;
-  description: string;
-  _id?: string;
-  arweave_tx_id?: string;  // Optional since it might not exist for new milestones
-}
 
 // Initialize MongoDB client
 const client = new MongoClient(process.env.MONGODB_URI!);
